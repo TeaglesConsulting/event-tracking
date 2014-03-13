007e337b38bb478c1b6b2ec8237436ed6f8bceb1
James T. Perreault <james@perreau.lt>
2014-03-12 21:37:51 -0400

    lookup project and write event

--- app/workers/write_worker.rb
+++ app/workers/write_worker.rb
@@ -2,8 +2,7 @@ class WriteWorker
   include Sidekiq::Worker
 
   def perform(event_params, project_key)
-    project = Project.where(key: project_key)
-    event_params[:project] = project
+    event_params[:project] = Project.find_by(key: project_key)
     Event.create!(event_params)
   end
 end
