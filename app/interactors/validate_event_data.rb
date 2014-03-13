1d4bc25ae0c6a8e5320da4c1aaf42eee0a266742
James T. Perreault <james@perreau.lt>
2014-03-12 21:38:40 -0400

    gut interactor, needs more thought

--- app/interactors/validate_event_data.rb
+++ app/interactors/validate_event_data.rb
@@ -1,12 +1,7 @@
 class ValidateEventData
   include Interactor
 
-  def perform()
-    #this lookup should be async
-    #fail!(errors: 'Project does not exist') if Project.where(key: two).none?
-
-    event = Event.new(context[:event_data])
-
-    fail!(errors: event.errors) if event.invalid?
+  def perform
+    
   end
 end
