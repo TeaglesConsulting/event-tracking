0aa88799f40214cc4a2aedc8cb374f35e976b4cd
James T. Perreault <james@perreau.lt>
2014-03-12 21:39:30 -0400

    comment initial setup due to write issues

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,29 @@
 class EventsController < ApplicationController
 
   def create
-    if ValidateEventData.perform(event_params).success?
-      WriteWorker.perform_async(event_params, project_key)
+    if Event.create!(event_params)
       head :accepted
     else
       head :bad_request
     end
+    # if ValidateEventData.perform(event_params).success?
+    #   WriteWorker.perform_async(event_params, project_key)
+    #   head :accepted
+    # else
+    #   head :bad_request
+    # end
   end
 
 private
   def event_params
-    { event_type: params[:event_type], data: params[:data] }
+    { event_type: params[:event_type], data: params[:data], project: project }
   end
 
   def project_key
     request.headers['authentication']
   end
 
+  def project
+    Project.find_by(key: project_key)
+  end
 end
