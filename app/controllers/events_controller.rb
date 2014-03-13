36cfdf0ff972eb6c5bc11f6261bd48ed90b19292
James T. Perreault <james@perreau.lt>
2014-03-13 10:28:14 -0400

    remove comments. queue worker on create. remove project lookup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,29 +1,17 @@
 class EventsController < ApplicationController
 
   def create
-    if Event.create!(event_params)
-      head :accepted
-    else
-      head :bad_request
-    end
-    # if ValidateEventData.perform(event_params).success?
-    #   WriteWorker.perform_async(event_params, project_key)
-    #   head :accepted
-    # else
-    #   head :bad_request
-    # end
+    WriteWorker.perform_async(event_params, project_key)
+    head :accepted
   end
 
 private
   def event_params
-    { event_type: params[:event_type], data: params[:data], project: project }
+    { event_type: params[:event_type], data: params[:data] }
   end
 
   def project_key
     request.headers['authentication']
   end
 
-  def project
-    Project.find_by(key: project_key)
-  end
 end
