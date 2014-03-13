2f3af0da1d3b31c4d4f3254214b2eb640007f56b
James T. Perreault <james@perreau.lt>
2014-03-13 10:27:28 -0400

    test needs project before running

--- spec/requests/events_requests_spec.rb
+++ spec/requests/events_requests_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe 'Events' do
 
-  let(:project) { create(:project, key: 'mykey', name: 'test site', url: 'tracker.dev') }
   let(:data) { '{ "foo" : "bar" }' }
 
   before :each do
     WriteWorker.jobs.clear
+    create(:project, key: 'mykey', name: 'test site', url: 'tracker.dev')
   end
 
   it 'queues a WriteWorker' do
+
     expect {
       post 'events/page_views', data, { 'Content-Type' => 'application/json', 'Authentication' => 'mykey'}
     }.to change(WriteWorker.jobs, :size).by(1)
